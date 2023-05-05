8fe25e37b1503db0ad255a86b7004e1bf9823df6
ksw06086 <k-sunwo@naver.com>
2023-05-06 04:55:50 +0900

    Suncloth_springboot: guest_category page complete

---
+++ suncloth/suncloth/src/main/java/com/suncloth/suncloth/controller/GuestController.java
@@ -0,0 +1,24 @@
+package com.suncloth.suncloth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/guest")
+public class GuestController {
+    @GetMapping("/category")
+    public String categoryPage(Model model
+            , @RequestParam(required = false) String category) {
+        if("outer".equals(category)){
+            System.out.println("model = " + model + ", category = " + category);
+        } else {
+            System.out.println("model = " + model + ", category = " + category);
+        }
+        model.addAttribute("category", category);
+        return "/guest/guest_category";
+    }
+
+}
