f3759e42e7fb8c0d883eec049555b882db81eec2
ksw06086 <k-sunwo@naver.com>
2023-05-11 10:07:08 +0900

    springboot_myhome: 회원가입 시 자동 가입인사

--- myhome/myhome/src/main/java/com/godcoder/myhome/service/UserService.java
+++ myhome/myhome/src/main/java/com/godcoder/myhome/service/UserService.java
@@ -1,7 +1,9 @@
 package com.godcoder.myhome.service;
 
+import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.model.Role;
 import com.godcoder.myhome.model.User;
+import com.godcoder.myhome.repository.BoardRepository;
 import com.godcoder.myhome.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,16 +16,30 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     public User save(User user){
         String encodePassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodePassword);
         user.setEnabled(true);
+
         Role role = new Role();
         role.setId(1);
         user.getRoles().add(role);
 
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+
+        // 사용자 가입인사글 자동작성
+        Board board = new Board();
+        board.setTitle("안녕하세요.");
+        board.setContent("반갑습니다.");
+        board.setUser(savedUser);
+        boardRepository.save(board);
+
+
+        return savedUser;
     }
 }
