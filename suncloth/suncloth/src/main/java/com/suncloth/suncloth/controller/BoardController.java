013634d7e0b1a7b63d175d2aa1ef1d1360acd214
ksw06086 <k-sunwo@naver.com>
2023-05-15 14:31:25 +0900

    suncloth_springboot: boardView(notice, FAQ) component complete

--- suncloth/suncloth/src/main/java/com/suncloth/suncloth/controller/BoardController.java
+++ suncloth/suncloth/src/main/java/com/suncloth/suncloth/controller/BoardController.java
@@ -10,10 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/board")
 public class BoardController {
     @GetMapping("/boardList")
-    public String noticeList(Model model
+    public String boardList(Model model
             , @RequestParam(required = false) String name) {
         model.addAttribute("name", name);
         return "/board/guest_boardList";
+    }
+
+    @GetMapping("/boardView")
+    public String boardView(Model model
+            , @RequestParam(required = false) String name) {
+        model.addAttribute("name", name);
+        return "/board/guest_boardView";
     }
 
 }
