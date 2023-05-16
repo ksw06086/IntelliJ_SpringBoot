18d3d9c09ec4f1e3a3a0980609c7aa3d525aae22
ksw06086 <k-sunwo@naver.com>
2023-05-16 20:26:26 +0900

    suncloth_springboot: profile component complete

--- suncloth/suncloth/src/main/java/com/suncloth/suncloth/controller/AccountController.java
+++ suncloth/suncloth/src/main/java/com/suncloth/suncloth/controller/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/account")
@@ -22,7 +23,9 @@ public class AccountController {
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model
+            , @RequestParam(required = false) String name) {
+        model.addAttribute("name", name);
         return "account/guest_register";
     }
 
