464cc4c26e89e8e4579e04fa3b6bf6d507a03b22
ksw06086 <k-sunwo@naver.com>
2023-05-22 22:56:19 +0900

    suncloth_springboot: host order, brand page complete

--- suncloth/suncloth/src/main/java/com/suncloth/suncloth/controller/HostController.java
+++ suncloth/suncloth/src/main/java/com/suncloth/suncloth/controller/HostController.java
@@ -9,20 +9,44 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/host")
 public class HostController {
+
+    /* 상품 */
+    // 상품 리스트
     @GetMapping("/productList")
     public String productList() {
-        return "/host/host_productList";
+        return "/host/product/host_productList";
     }
-
+    // 상품 등록
     @GetMapping("/productInput")
     public String productInput() {
-        return "/host/host_productInput";
+        return "/host/product/host_productInput";
     }
-
+    // 상품 수정
     @GetMapping("/productView")
     public String productView(Model model
             , @RequestParam(required = false) String name) {
         model.addAttribute("name", name);
-        return "/host/host_productView";
+        return "/host/product/host_productView";
+    }
+
+    /* 브랜드 */
+    // 브랜드 목록
+    @GetMapping("/brandList")
+    public String brandList() {
+        return "/host/product/host_brandList";
+    }
+    // 브랜드 등록/수정
+    @GetMapping("/brandInput")
+    public String brandInput(Model model
+            , @RequestParam(required = false) String name) {
+        model.addAttribute("name", name);
+        return "/host/product/host_brandInput";
+    }
+
+    /* 주문 */
+    // 주문 목록
+    @GetMapping("/orderList")
+    public String orderList() {
+        return "/host/order/host_orderList";
     }
 }
