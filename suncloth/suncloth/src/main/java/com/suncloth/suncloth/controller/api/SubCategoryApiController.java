f71bfba22166c29c096a3193e172a1682cdb7a73
ksw06086 <k-sunwo@naver.com>
2023-05-31 17:55:35 +0900

    Suncloth Springboot: 상품등록 - MainCategoryApiController, SubCategoryApiController 생성

---
+++ suncloth/suncloth/src/main/java/com/suncloth/suncloth/controller/api/SubCategoryApiController.java
@@ -0,0 +1,84 @@
+package com.suncloth.suncloth.controller.api;
+
+import com.suncloth.suncloth.model.MainCategory;
+import com.suncloth.suncloth.model.SubCategory;
+import com.suncloth.suncloth.repository.MainCategoryRepository;
+import com.suncloth.suncloth.repository.SubCategoryRepository;
+import io.micrometer.common.util.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class SubCategoryApiController {
+    @Autowired
+    private final SubCategoryRepository repository;
+
+    @Autowired
+    private final MainCategoryRepository mainCategoryRepository;
+
+    SubCategoryApiController(SubCategoryRepository repository, MainCategoryRepository mainCategoryRepository) {
+        this.repository = repository;
+        this.mainCategoryRepository = mainCategoryRepository;
+    }
+
+    // GET : SubCategory 테이블 정보 가져오기
+    @GetMapping("/subCategories")
+    List<SubCategory> all(@RequestParam(required = false, defaultValue = "") String subName) {
+        if(StringUtils.isEmpty(subName)){
+            return repository.findAll();
+        } else {
+            return repository.findBySubName(subName);
+        }
+    }
+    // end::get-aggregate-root[]
+
+    // POST : SubCategory 테이블에 정보 삽입하기
+    @PostMapping("/subCategory")
+    SubCategory newSubCategory(@RequestBody SubCategory newSubCategory) {
+        System.out.println(newSubCategory);
+        return repository.save(newSubCategory);
+    }
+
+    // Single item
+
+    // GET : mainCode 에 맞게 한가지 Sub Category 정보만 가져오기
+    @GetMapping("/subCategories/{mainCode}")
+    List<SubCategory> oneAndMany(@PathVariable Long mainCode) {
+        MainCategory mainCategory = mainCategoryRepository.findById(mainCode).orElse(null);
+        return repository.findByMainCategory(mainCategory);
+    }
+
+    // GET : Id에 맞게 한가지 Sub Category 정보만 가져오기
+    @GetMapping("/subCategory/{subCode}")
+    SubCategory one(@PathVariable Long subCode) {
+        return repository.findById(subCode).orElse(null);
+    }
+
+    // PUT : Id에 맞게 한가지 Sub Categroy 정보만 갱신
+    @PutMapping("/subCategories/{subCode}")
+    SubCategory replaceMainCategory(@RequestBody SubCategory newSubCategory, @PathVariable Long subCode) {
+
+        return repository.findById(subCode)
+                .map(subCategory -> {
+                    subCategory.setSubName(newSubCategory.getSubName());
+                    return repository.save(subCategory);
+                })
+                .orElseGet(() -> {
+                    newSubCategory.setSubCode(subCode);
+                    return repository.save(newSubCategory);
+                });
+    }
+
+    // 관리자일 경우에만 삭제가 가능하고, ID에 맞는 한가지의 SubCategory 만 삭제
+    @Secured("ROLE_ADMIN")
+    @DeleteMapping("/subCategories/{subCode}")
+    void deleteSubCategory(@PathVariable Long subCode) {
+        repository.deleteById(subCode);
+    }
+}
--- suncloth/suncloth/src/main/java/com/suncloth/suncloth/repository/SubCategoryRepository.java
+++ suncloth/suncloth/src/main/java/com/suncloth/suncloth/repository/SubCategoryRepository.java
@@ -4,6 +4,12 @@ import com.suncloth.suncloth.model.MainCategory;
 import com.suncloth.suncloth.model.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+import java.util.List;
 
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
+    // Sub Category 이름을 기준으로 정보들 가져오기(where sub_name = ?1)
+    List<SubCategory> findBySubName(String subName);
+
+    // Main Category 를 기준으로 정보들 가져오기(where main_code = ?1)
+    List<SubCategory> findByMainCategory(MainCategory mainCategory);
 }
